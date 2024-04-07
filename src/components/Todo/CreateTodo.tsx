a3b1b5b0b30aee1b99b1927e35efbbc061741489
Jay Mingle <jkawudi@gmail.com>
2024-04-07 19:14:50 +0000

    Changes

--- src/components/Todo/CreateTodo.tsx
+++ src/components/Todo/CreateTodo.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import {Box, TextField} from "@mui/material";
+import {Box, Button, styled, TextField} from "@mui/material";
 
 const CreateTodo: React.FC= () => {
-    return (
-        <Box>
-            <TextField autoFocus id="outlined-basic" placeholder="Enter To Do" variant="outlined" />
 
-        </Box>
+    const StyledBox = styled(Box)({
+        display: 'flex',
+        justifyContent: 'center',
+        gap: '40px',
+        padding: '20px 0'
+    })
+
+    return (
+        <StyledBox>
+            <TextField sx={{width: '60%'}} autoFocus id="outlined-basic" placeholder="Enter To Do" variant="standard" />
+            <Button variant="contained">Add Todo</Button>
+        </StyledBox>
     );
 };
 
