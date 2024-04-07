5a5b40dd9bece7f45d6df016709cd0a8636fc4e4
Jay Mingle <jkawudi@gmail.com>
2024-04-07 20:35:19 +0000

    Changes

--- src/components/Todo/TodoContainer.tsx
+++ src/components/Todo/TodoContainer.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CreateTodo from "./CreateTodo.tsx";
 import TodoLists from "./TodoLists.tsx";
+import Todo from "../../model.ts";
 
-const TODO_LISTS = [
+const TODO_LISTS: Todo[] = [
     {id: 1, todo: 'Cook', completed: false},
     {id: 2, todo: 'Clean My Room', completed: false},
     {id: 3, todo: 'Go Grocery Shopping', completed: false},
 ]
 
 const TodoContainer: React.FC = () => {
+
+    const [todoList, setTodoList] = useState<Todo[]>(TODO_LISTS);
+    console.log('Todo Container: ', todoList)
     return (
         <div>
             <CreateTodo/>
