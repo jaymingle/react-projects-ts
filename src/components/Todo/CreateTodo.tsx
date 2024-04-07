7a21792d10d71f395c8db5d85abecf36ecf230b1
Jay Mingle <jkawudi@gmail.com>
2024-04-07 21:23:38 +0000

    Changes

--- src/components/Todo/CreateTodo.tsx
+++ src/components/Todo/CreateTodo.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Box, Button, styled, TextField} from "@mui/material";
+import Todo from "../../model.ts";
 
 const CreateTodo: React.FC= () => {
+
+    const[todo, setTodo] = useState<string>('');
 
     const StyledBox = styled(Box)({
         display: 'flex',
@@ -10,10 +13,18 @@ const CreateTodo: React.FC= () => {
         padding: '20px 0'
     })
 
+    const todoSubmitHandler = (e) => {
+        e. preventDefault();
+        console.log('Clicked')
+        // console.log(todo)
+        // setTodo('')
+    }
+
     return (
         <StyledBox>
-            <TextField sx={{width: '70%'}} autoFocus id="outlined-basic" placeholder="Enter To Do" variant="standard" />
-            <Button variant="contained">Add Todo</Button>
+            <form onSubmit={todoSubmitHandler}></form>
+            <TextField sx={{width: '70%'}} value={todo} onChange={e => setTodo(e.target.value)} autoFocus id="outlined-basic" placeholder="Enter To Do" variant="standard" />
+            <Button type="submit" variant="contained">Add Todo</Button>
         </StyledBox>
     );
 };
