cee801a20c014c5a4a2062deccced6d16a80f883
Jay Mingle <jkawudi@gmail.com>
2024-04-07 19:03:14 +0000

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import TodoContainer from "./components/Todo/TodoContainer.tsx";
+import {Box} from "@mui/material";
 
 function App() {
 
-  return (
-    <>
+  const style =  {
+    backgroundColor: 'yellow',
+    width: '70%',
+    margin: 'auto',
+    marginTop: '40px',
+  }
 
-    </>
+  return (
+    <Box style={style}>
+        <TodoContainer/>
+    </Box>
   )
 }
 
