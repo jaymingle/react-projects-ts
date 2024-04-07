436b13150de5cc208f674bcff539665499df7afa
Jay Mingle <jkawudi@gmail.com>
2024-04-07 20:24:30 +0000

    Changes

--- src/components/Todo/TodoContainer.tsx
+++ src/components/Todo/TodoContainer.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import CreateTodo from "./CreateTodo.tsx";
 import TodoLists from "./TodoLists.tsx";
 
+const TODO_LISTS = [
+    {id: 1, todo: 'Cook', isDone: false},
+    {id: 2, todo: 'Clean My Room', isDone: false},
+    {id: 3, todo: 'Go Grocery Shopping', isDone: false},
+]
+
 const TodoContainer: React.FC = () => {
     return (
         <div>
